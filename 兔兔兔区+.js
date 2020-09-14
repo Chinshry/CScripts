e7f8b47cb15a42e6dfdaa7f66a9ac7271ac5956d
chinshry <cs616520704@gmail.com>
2020-09-15 01:14:43 +0800

    Add Script for 兔区/晋江

--- 兔兔兔区+.js
+++ 兔兔兔区+.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         兔兔兔区+
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  屏蔽用户帖子|发帖记录|快捷举报|楼主标记|只看楼主|只看TA|白色主题去广告|链接可点击|顶部底部直达
 // @author       cccccc
 // @include      https://bbs.jjwxc.net/bindex.php*
